6a17255781be076d7b896dcb4bedcd36a9540b96
estebanlimono <42361507+estebanlimono@users.noreply.github.com>
2018-08-23 08:44:47 -0500

    Revert "laboratorio 1 version final"
    This reverts commit 276e2a46fc1ce36ba69ef44681d146190a924ede.

--- laboratorio 1.R
+++ laboratorio 1.R
@@ -2,9 +2,9 @@
 #install.packages('knitr', dependencies = TRUE)
 suppressMessages(library(plotly))#graficas iterativas
 suppressMessages(library(Quandl))#Descargar Precios
-suppressMessages(library(PortfolioAnalytics))#Teoria Moderna de portafolios
+suppressMessages(library(PortafolioAnalytics))#Teoria Moderna de portafolios
 suppressMessages(library(ROI))#Optimizacion de portafolio
-suppressMessages(library(knitr))#opciones de documentacion + codigo
+suppressMessages(library(kintr))#opciones de documentacion + codigo
 suppressMessages(library(kableExtra)) # Tablas en HTML
 options(kintr.table.format="html")
 
@@ -12,7 +12,7 @@ options(kintr.table.format="html")
 Quandl.api_key("XrMP9jQysz1Z_6DdWgfi")
 
 #Funcion para bajar precios
-Bajar_Precios <-function(Columns,Tickers,Fecha_In,Fecha_Fn) {
+Bajar_Precos <-function(Columns,Tickers,Fecha_In,Fecha_Fn) {
   #Funcion para descargar N cantidad de activos desde QUANDL
   #--Dependencias:QUANDL
   #--Columns:columnas a incluir:c"date","adj_close")
@@ -24,7 +24,7 @@ Bajar_Precios <-function(Columns,Tickers,Fecha_In,Fecha_Fn) {
 
 #Tickers de acciones y datos a solicitar a QANDL
 tk<-c("TSLA","BBY","HD")
-cs<-c("date","adj_close")
+CS<-c("date","adj_close")
 
 #Fechas inicial y final
 fs<- c("2016-08-01","2018-08-01")
@@ -48,4 +48,3 @@ Rends <- xts(x = cbind(Datos[[1]]$adj_close_r, Datos[[2]]$adj_close_r, Datos[[3]
 names(Rends) <- tk
 #los resultados se ven como xts
 #datos[3 es donde se ponen la cantidad de columnas]
-
