e7979de35a6073a261d111d45f52f6d405f63e3e
estebanlimono <42361507+estebanlimono@users.noreply.github.com>
2018-09-03 07:59:03 -0500

    se hicieron portafolios aleatorios
    clase 03-09-2018

--- laboratorio 1.R
+++ laboratorio 1.R
@@ -65,3 +65,16 @@ Port1<-add.objective(portfolio=Port1,type="return",name="mean")
 
 Port1<-optimize.portfolio(R=Rends,portfolio=Port1,optimize_method ="random",
                           trace=TRUE,search_size=50)
+#Port1$random_portfolio_objective_results
+Portafolios<-vector("list",
+                    length=length(Port1$random_portfolio_objective_results))
+for(i in 1:length(Port1$random_portfolio_objective_results)){
+  Portafolios[[i]]$Pesos<-Port1$random_portfolio_objective_results[[i]]$weights
+  Portafolios[[i]]$Medias<-Port1$random_portfolio_objective_results[[i]]$objective_measures$mean
+  Portafolios[[i]]$Vars<-var.portfolio(R=Port1$R,weights=Portafolios[[i]]$Pesos)
+  names(Portafolios[[i]]$Medias)<-NULL
+}
+
+df_Portafolios<-data.frame(matrix(nrow=length(Port1$random_portfolio_objective_results),ncol=3,data=0))
+
+colnames(df_Portafolios)<-c("Rend","Var","Clase")
