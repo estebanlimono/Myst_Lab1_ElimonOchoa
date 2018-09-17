a8b33edbf6305634d0c3457829037bcad0dfa2ec
estebanlimono <42361507+estebanlimono@users.noreply.github.com>
2018-09-17 08:48:08 -0500

    update 17092018

--- laboratorio 2.R
+++ laboratorio 2.R
@@ -1,367 +1,116 @@
-# Remover todos los objetos del "Environment"
+#LABORATORIO 2
 
+# Remover todos los objetos del "Environment"
 rm(list = ls())
 
-
-
 # los 0s aceptados antes de expresas una cifra en notaci?n cient?fica
-
 options("scipen"=100, "digits"=4)
 
-
-
 ### Cargas librer?as a utilizar
-
 suppressMessages(library(plotly)) # Graficas interactivas
-
-suppressMessages(library(Quandl)) # Descargar Precios
-
+library(Quandl) # Descargar Precios
 suppressMessages(library(PortfolioAnalytics)) # Teor?a Moderna de Portafolios
-
 suppressMessages(library(ROI)) # Optimizacion para portafolio
 
-
-
 suppressMessages(library(knitr))  # Opciones de documentaci?n + c?digo
-
 suppressMessages(library(kableExtra)) # Tablas en HTML
+#suppressMessages(library(xlsx))
+suppressMessages(library(openxlsx))
 
 options(knitr.table.format = "html") 
 
-
-
 # Cargar el token de QUANDL
-
 Quandl.api_key("dN9QssXxzTxndaqKUQ_i")
-
-Capital_Inicial <- 10000
-
 
 
 # Funcion para descagar precios
-
 Bajar_Precios <- function(Columns, Tickers, Fecha_In, Fecha_Fn) {
-  
-  
-  
-  # Funcion para descargar N cantidad de activos desde QUANDL
-  
-  # -- Dependencias: QUANDL
-  
-  # -- Columns : columnas a incluir : character : c("date", "adj_close", ... )
-  
-  # -- Tickers : Tickers o claves de pizarra de los activos : character : "TSLA"
-  
-  # -- Fecha_In : Fecha Inicial : character : "2017-01-02"
-  
-  # -- Fecha_Fn : Fecha Final : character : "2017-08-02"
-  
-  
-  
-  # Peticion para descargar precios
-  
   Datos <- Quandl.datatable("WIKI/PRICES", qopts.columns=Columns, ticker=Tickers,
-                            
                             date.gte=Fecha_In, date.lte=Fecha_Fn)
-  
-  return(Datos)
-  
-}
+  return(Datos)}
 
 
-
-# Tickers de accciones y datos a solicitar a QUANDL
-
-tk <- c("AMZN", "AAPL", "WMT")
-
-cs <- c("date", "adj_close")
-
+#Tickers de acciones en ETF-IAK
+#IAK <- read_excel("trading/Myst_Lab1_ElimonOchoa/IAK.xlsx")
+# tk<-as.data.frame(read.xlsx(file="IAK.xlsx", sheetName="Holdings",
+#                             colIndex=1,sartRow=10,endRow=73,
+#                             header=FALSE))
+#IAK<- read.xlsx("IAK.xlsx", sheet = 1)
+Datos_ETF<- read.xlsx("IAK.xlsx" , sheet=1)
+tk<- as.character(na.omit(Datos_ETF[which(Datos_ETF[,1]== "Ticker")+1:length(Datos_ETF[,1]),1]))
+cs<- c("date", "adj_close")
 
 
 # Fecha inicial y fecha final
+fs <- c("2015-08-01", "2017-08-01")
 
-fs <- c("2017-03-01", "2018-03-01")
-
-
-
-# Descargar Precios y Calcular rendimientos
-
+# Descargar Precios
 Datos <- list()
 
-
-
 for(i in 1:length(tk))
-  
   Datos[[i]] <- Bajar_Precios(Columns=cs, Ticker=tk[i], Fecha_In=fs[1], Fecha_Fn=fs[2])
 
-
-
 names(Datos) <- tk
-#Ordenar datos de 1 a ultimo
-for(i in 1:length(tk)){
-  Datos[[i]]<-Datos[[i]][order(Datos[[i]][,1]), ]
+
+longitudes<-c()
+
+for (i in 1: length(Datos)){
+  longitudes[[i]] <- length(Datos[[i]]$date)
 }
 
-for(i in 1:length(tk))
-  
-  Datos[[i]]$adj_close_r <- c(0, diff(log(Datos[[i]]$adj_close)))
+maximo <- max(longitudes)
+completos <- which(longitudes== maximo)
+DatosN <- Datos[completos]
 
+#vector para almacenar columnas
+columnas<- c()
+nuevos <- c()
 
+#funcion para repetir una funcion por cada columna del data.frame
+Precios<- do.call(cbind, DatosN)
 
-Rends <- xts(x = cbind(Datos[[1]]$adj_close_r, Datos[[2]]$adj_close_r, Datos[[3]]$adj_close_r),
-             
-             order.by = Datos[[1]]$date)[-1]
-
-names(Rends) <- tk
-
-
-
-Port1 <- portfolio.spec(assets=tk)
-
-Port1 <- add.constraint(portfolio=Port1,
-                        
-                        type="full_investment")
-
-
-
-# Restriccion 2: Limites superior e inferior para el valor de los pesos individuales
-
-Port1 <- add.constraint(portfolio=Port1,
-                        
-                        type="box", 
-                        
-                        min=c(0.1, 0.1, 0.1), max=c(0.7, 0.7, 0.7))
-
-
-
-Port1 <- add.objective(portfolio = Port1, type = "return", name = "mean")
-
-
-
-Port1 <- optimize.portfolio(R=Rends, portfolio=Port1, optimize_method="random",
-                            
-                            trace=TRUE, search_size=10000)
-
-
-
-Portafolios <- vector("list",
-                      
-                      length = length(Port1$random_portfolio_objective_results))
-
-
-
-for(i in 1:length(Port1$random_portfolio_objective_results)) {
-  
-  
-  
-  Portafolios[[i]]$Pesos  <- Port1$random_portfolio_objective_results[[i]]$weights
-  
-  Portafolios[[i]]$Medias <- Port1$random_portfolio_objective_results[[i]]$objective_measures$mean
-  
-  
-  
-  Portafolios[[i]]$Vars   <- var.portfolio(R = Port1$R, weights = Portafolios[[i]]$Pesos)
-  
-  names(Portafolios[[i]]$Medias) <- NULL
-  
+#crear vector con nombres de columnas de interes 
+for (i in 1:length(tk)){
+  nuevos[i]<- paste(tk[i], ".adj_close", sep="")
 }
 
+#extraer 1 renglon para obtener para obtener los nombres de las columnas
+nombres <- colnames(Precios[1, (names(Precios) %in% nuevos)])
+
+# Elejir una columna date y las demas columnas de rendimientos
+Precios <- Precios[,(names(Precios) %in% nuevos)]
+row.names(Precios) <- DatosN[[1]]$date
+
+# Reasignar nombres al data.frame
+tk_completos <- as.character(tk[completos])
+colnames(Precios) <- tk_completos 
 
 
-df_Portafolios <- data.frame(matrix(nrow=length(Port1$random_portfolio_objective_results),
-                                    
-                                    ncol=3,
-                                    
-                                    data = 0))
+Historico <- data.frame("Date"= row.names(Precios),
+                        "Precio"= Precios[,1],
+                        "R_Precio" = 0,
+                        "R_Activo" = 0,
+                        "R_Cuenta" = 0,
+                        "Capital" = 0, "Balance" = 0, "Titulos" = 0,
+                        "Titulos_a" = 0,
+                        "Operacion" = NA, "Comisiones"= 0, "Mensaje" = NA)
 
-colnames(df_Portafolios) <- c("Rend","Var","Clase")
+#Date       : Fecha (proviene desde los precios que bajaron)
+#Precio     : Precio individual del activo
+#R_Precio   : Rendimiento diario del precio (dia a dia)
+#R_Activo   : Rendimiento acumulado del precio (Cada dia respecto al precio inicial)
+#Capital    : El dinero no invertido (equivalente a efectivo)
+#Balance    : El valor del portafolio (precio diario X titulos)
+#R_Cuenta   : Balance + Capital (cada dia respecto al capital inicial)
+#Titulos    : Acciones que se tienen
+#Titulos_a  : Titulos acumulados
+#Operacion  : Indicativo de compra (1), mantener (0), venta (-1)
+#Comisiones : 0.0025 o 0.25% por el valor de la transaccion
+#Mensaje    : Un texto que indique alguna decision o indicativo de que ocurrio algo
 
-
-
-for(i in 1:length(Port1$random_portfolio_objective_results)) {
-  
-  
-  
-  df_Portafolios$Rend[i]  <- round(Portafolios[[i]]$Medias*252,4)
-  
-  df_Portafolios$Var[i]   <- round(sqrt(Portafolios[[i]]$Vars)*sqrt(252),4)
-  
-  df_Portafolios$Clase[i] <- "No-Frontera"
-  
-  
-  
-  for(k in 1:length(tk)) {
-    
-    
-    
-    df_Portafolios[i,paste("Peso_", tk[k], sep="")] <- Portafolios[[i]]$Pesos[k]
-    
-    
-    
-    df_Portafolios[i,paste("Titulos_ini_", tk[k],sep="")] <-
-      
-      (Capital_Inicial*Portafolios[[i]]$Pesos[k])%/%Datos[[k]]$adj_close[1]
-    
-  }
-  
-}
-
-
-
-Plot_portafolios <- plot_ly(x=df_Portafolios$Var, y=df_Portafolios$Rend, type='scatter', mode='markers',
-                            
-                            name = "Portafolios", marker = list(color="grey", size=7), hoverinfo='text', 
-                            
-                            text = ~paste('Rendimiento: ', paste(df_Portafolios$Rend*100, "%") ,
-                                          
-                                          '<br> Riesgo: ', paste(df_Portafolios$Var*100, "%") )) %>% 
-  
-  layout(title = "Portafolios (Markowitz)",
-         
-         xaxis = list(title = "Riesgo (Desviaci?n Est?ndar Anualizada)",
-                      
-                      showgrid = F),
-         
-         yaxis = list(title = "Valor Esperado (Rendimiento Anualizado)"),
-         
-         legend = list(orientation = 'h', y = -0.25))
-
-Plot_portafolios
-
-
-
-Port_1 <- df_Portafolios[which.max(df_Portafolios$Rend),]
-
-
-
-# Portafolio con m?nima varianza
-
-Port_2 <- df_Portafolios[which.min(df_Portafolios$Var),]
-
-
-
-# Tasa libre de riesgo
-
-rf <- 0.0025          
-
-# Rendimiento de portafolio
-
-rp <- df_Portafolios$Rend
-
-# Varianza de portafolio
-
-sp <- df_Portafolios$Var
-
-# Indice de sharpe
-
-sharpe <- (rp-rf)/sp
-
-
-
-# Portafolio con m?ximo Sharpe ratio 
-
-Port_3 <- df_Portafolios[which.max(sharpe),]
-
-
-
-Ports <- cbind(rbind(Port_1, Port_2, Port_3),
-               
-               "Portafolio" = c("M?ximo Rendimiento","M?nima Varianza","M?ximo Sharpe Ratio"))
-
-
-
-Plot_portafolios <- plot_ly(x=df_Portafolios$Var, y=df_Portafolios$Rend, type='scatter', mode='markers',
-                            
-                            name = "Portafolios", marker = list(color="grey", size=7), hoverinfo='text', 
-                            
-                            text = ~paste('Rendimiento: ', paste(df_Portafolios$Rend*100, "%") ,
-                                          
-                                          '<br> Riesgo: ', paste(df_Portafolios$Var*100, "%") )) %>% 
-  
-  layout(title = "Portafolios (Markowitz)",
-         
-         xaxis = list(title = "Riesgo (Desviaci?n Est?ndar Anualizada)",
-                      
-                      showgrid = F),
-         
-         yaxis = list(title = "Valor Esperado (Rendimiento Anualizado)"),
-         
-         legend = list(orientation = 'h', y = -0.25)) %>%
-  
-  add_trace(x = ~Ports$Var[1], y = ~Ports$Rend[1], name = Ports$Portafolio[1],
-            
-            mode = 'marker', marker = list(color="red", size=10)) %>%
-  
-  add_trace(x = ~Ports$Var[2], y = ~Ports$Rend[2], name = Ports$Portafolio[2],
-            
-            mode = 'marker', marker = list(color="blue", size=10)) %>%
-  
-  add_trace(x = ~Ports$Var[3], y = ~Ports$Rend[3], name = Ports$Portafolio[3],
-            
-            mode = 'marker', marker = list(color="orange", size=10))
-
-Plot_portafolios
-
-
-
-# Pesos y titulos iniciales, de todos los activos, para los 3 portafolios
-
-Pesos_Titulos <- Ports[,-c(1,2,3)]
-
-
-
-# Encontrar las columnas cuyo nombre contenga "Titulos_ini", con esas encontraremos m?s f?cil los t?tulos
-
-# por portafolio por activo
-
-Ind <- grep(pattern = "Titulos_ini",x = colnames(Pesos_Titulos))
-
-Historicos_Ports <- data.frame("Date" = Datos[[1]]$date)
-
-
-
-# Crear data frame que contendr? los datos finales de cada estrategia
-
-for(i in 1:length(Ports[,1])) {
-  
-  Historicos_Ports[[paste("Portafolio_",i,sep="")]] <- 
-    
-    (Datos[[1]]$adj_close*Pesos_Titulos[i,Ind[1]]  + 
-       
-       Datos[[2]]$adj_close*Pesos_Titulos[i,Ind[2]] +
-       
-       Datos[[3]]$adj_close*Pesos_Titulos[i,Ind[3]])
-  
-}
-
-
-
-
-
-plot_ly(Historicos_Ports) %>%
-  
-  add_trace(x = ~Date, y = ~round(Portafolio_1,2), type = 'scatter', mode = 'lines', name = 'M?ximo Rendimiento',
-            
-            line = list(color = 'red'), hoverinfo = "text", text = ~paste('Port_1',round(Portafolio_1,2))) %>%
-  
-  add_trace(x = ~Date, y = ~round(Portafolio_2,2), type = 'scatter', mode = 'lines', name = 'M?nima Varianza',
-            
-            line = list(color = 'blue'), hoverinfo = "text", text = ~paste('Port_2',round(Portafolio_2,2)))  %>%
-  
-  add_trace(x = ~Date, y = ~round(Portafolio_3,2), type = 'scatter', mode = 'lines', name = 'M?ximo Sharpe Ratio',
-            
-            line = list(color = 'orange'), hoverinfo = "text", text = ~paste('Port_3',round(Portafolio_3,2)))%>% 
-  
-  layout(title = "3 Portafolios distintos objetivos",
-         
-         xaxis = list(title = "Fechas", showgrid = T),
-         
-         yaxis = list(title = "Balance"), 
-         
-         legend = list(orientation = 'h', y = -0.25, x = 0.5))
-#Para analizar este portafolio de activos los escogí en base a empresas que hayan tenido buenos resultados financieros.
-#La teoría de inversión pasiva es buena y el portafolio de Markowitz te ayuda a elegir las mejores ponderaciones según tu estilo de inversión.
-#Al generar 10,000 portafolios aleatorios con diferentes ponderaciones se puede elegir el que te maneje mejor perfil según tu elección.
-#Se debe de saber qe la teoría de portafolios de inversión moderna trabaja con datos pasados, por lo que también se deben de considerar otras variables antes de invertir.
-#El portafolio de Markowitz azul te da el mayor rendimiento ante el mínimo riesgo, este se recomienda a personas que esten invirtiendo parte de su fondo de retiro.
-#El portafolio amarillo te demuestra el mayor ratio sharpe o mayor esperanza del portafolio restandole la tasa libre de riesgo y dividiendolo entre la desviación del activo.
+Regla0_R <- -0.03  # Considerar una oportunidad de compra en un rendimiento de -3% o menor
+Regla1_I <- 0.20   # % de capital para comprar titulos para posicion inicial
+Regla2_P <- 0.25   # Se utiliza el P% del L capital restante en cada compra
+Regla3_W <- tk_completos # Se realiza la misma estrategia para todos los activos en el portafolio
+Regla4_C <- 0.0025 # Comisiones pagadas por compra
+Regla5_K <- 100000 # Capital inicial
